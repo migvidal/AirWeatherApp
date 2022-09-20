d22bab62f061242020a627037dd9633d74d11611
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2022-09-20 21:34:10 +0200

    Complete: ask for location permissions

--- app/src/main/java/com/example/airweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/airweatherapp/MainActivity.kt
@@ -1,29 +1,122 @@
 package com.example.airweatherapp
 
+import android.Manifest
 import android.app.SearchManager
 import android.content.ComponentName
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.Menu
-import android.view.View
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.core.app.ActivityCompat
 import com.example.airweatherapp.databinding.ActivityMainBinding
-import com.example.airweatherapp.databinding.ActivitySearchBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
+import java.util.logging.Logger
 
-class MainActivity : AppCompatActivity() {
+const val PERMISSION_REQUEST_LOCATION = 1000
 
+class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+    /**
+     * Location client
+     */
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    /**
+     * Location
+     */
+    private lateinit var location: Location
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // layout
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+
+        // Listener for button
+        binding.btnShowLocation.setOnClickListener {
+            showLocation()
+        }
+
     }
+
+    private fun showLocation() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            getLocation()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                PERMISSION_REQUEST_LOCATION
+            )
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_LOCATION)
+        }
+    }
+
+    private fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            val lat = it.latitude
+            binding.tvLocation.text = lat.toString()
+        }
+
+
+        /*val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    fusedLocationClient.lastLocation.addOnSuccessListener {
+                        val lat = it.latitude
+                    }
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+        requestPermissionLauncher.launch("Input")*/
+       }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,6 +141,24 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_search -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            // Request for camera permission.
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted
+                getLocation()
+            } else {
+                // Permission request was denied.
+
+            }
         }
     }
 }
