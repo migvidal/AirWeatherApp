5d85afd492c7df0a3460af2fdf96dfa2ac8b4eba
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2022-09-20 17:46:58 +0200

    Set up search activity and app bar

--- app/src/main/java/com/example/airweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/airweatherapp/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.airweatherapp
 
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import com.example.airweatherapp.databinding.ActivityMainBinding
+import com.example.airweatherapp.databinding.ActivitySearchBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        // Search elements
+        val searchItem = menu.findItem(R.id.action_search) //Search menu item
+        val search: View = searchItem.actionView
+        val searchView = search as SearchView //The associated embedded SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        // The class that will perform the search for this activity
+        val componentName = ComponentName(this, SearchActivity::class.java)
+        // Set the searchable info
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_search -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
